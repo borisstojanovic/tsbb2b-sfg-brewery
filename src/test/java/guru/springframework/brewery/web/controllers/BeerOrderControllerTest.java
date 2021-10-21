5e65d1ed2a0c1446b5dcb5e19c1718055d5015fe
boris.stojanovic <boris.stojanovic@msg-global.com>
2021-10-21 14:20:21 +0200

    Started Assignment - write spring mvc tests to get orders

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerOrderControllerTest.java
@@ -0,0 +1,16 @@
+package guru.springframework.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerOrderControllerTest {
+
+    @Test
+    void listOrders() {
+    }
+
+    @Test
+    void getOrder() {
+    }
+}
